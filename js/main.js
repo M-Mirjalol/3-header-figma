db653b0588478b47fa03934a5010748b3308b4ba
Your Name <you@example.com>
2025-11-22 15:38:43 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const burger1 = document.getElementById('burger1');
+const nav1 = document.getElementById('nav_links1');
+burger1.addEventListener('click', () => {
+    nav1.classList.toggle('open');
+});
+
+const burger2 = document.getElementById('burger2');
+const nav2 = document.getElementById('nav_links2');
+burger2.addEventListener('click', () => {
+    nav2.classList.toggle('open');
+});
+
+
+const burger3 = document.getElementById('burger3');
+const nav3 = document.getElementById('nav_links3');
+burger3.addEventListener('click', () => {
+    nav3.classList.toggle('open');
+});
